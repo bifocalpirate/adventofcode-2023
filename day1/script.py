452f65e57accd070065e4a785b332b6a9859870e
mirrus <sanserif8pt@gmail.com>
2023-12-02 11:28:59 +0200

    day1

---
+++ day1/script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import regex
+running_total = 0
+with open("/home/rolex/dev/adventofcode-2023/day1/data.txt","r") as f:
+    for line in f:
+        r = regex.findall("([0-9]*)",line)                
+        s = "".join(r)        
+        v = int(s[0] + s[-1])
+        running_total += v        
+print(running_total)
